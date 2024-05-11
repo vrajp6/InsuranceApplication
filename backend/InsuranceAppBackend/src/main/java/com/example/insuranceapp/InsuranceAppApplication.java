93bc1376d4adffb80aded7996a9cfd99aa9227a4
vrajp6 <vrajpatel2021@gmail.com>
2024-05-11 00:39:31 -0500

    Initial commit - project setup

---
+++ backend/InsuranceAppBackend/src/main/java/com/example/insuranceapp/InsuranceAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.insuranceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceAppApplication.class, args);
+    }
+}
